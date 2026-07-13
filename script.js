b8f891de55d8fd58c96e246621db77e2f1d439e0
barak262626 <bl262626@gmail.com>
2026-07-13 10:01:27 +0300

    Add weekly salesman discount QNA

--- script.js
+++ script.js
@@ -1680,7 +1680,7 @@ function initializeWeeklyQuestionAlert() {
     }
 
     const loader = document.createElement("script");
-      loader.src = "weekly-qna.js?v=20260712-pizza-scooter-republish-1";
+      loader.src = "weekly-qna.js?v=20260713-salesman-discount-accessory-1";
     loader.addEventListener("load", showWeeklyQuestionAlert);
     document.head.append(loader);
   });
