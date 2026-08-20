b6eb7732a59e83cfa73f9f2dc28547313a00aa5f
barak262626 <bl262626@gmail.com>
2026-08-20 16:09:03 +0300

    Add weekly orphan and vitaminchik QNA

--- script.js
+++ script.js
@@ -1810,7 +1810,7 @@ function initializeWeeklyQuestionAlert() {
     }
 
     const loader = document.createElement("script");
-      loader.src = "weekly-qna.js?v=20260820-tefillin-heter-iska-1";
+      loader.src = "weekly-qna.js?v=20260820-yatom-vitamintchik-1";
     loader.addEventListener("load", showWeeklyQuestionAlert);
     document.head.append(loader);
   });
